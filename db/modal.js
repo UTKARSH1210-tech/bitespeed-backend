530da2c4a2e97b35b2f67898c7c9b26c5ec80c54
UTKARSH1210-tech <iec2020060@iiita.ac.in>
2024-05-31 11:43:43 +0530

    postgres service updated

--- db/modal.js
+++ db/modal.js
@@ -1,8 +1,13 @@
 // const { Sequelize, DataTypes, Op } = require('sequelize');
 import { Sequelize, DataTypes , Op} from 'sequelize';
 
-export const sequelize = new Sequelize('bitespeedDB', 'postgres', '1234', {
-  host: 'localhost',
+// export const sequelize = new Sequelize('bitespeedDB', 'postgres', '1234', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
+
+export const sequelize = new Sequelize('postgres_service_nst5', 'postgres_service_nst5_user', 'rEkNxBelKINzMxKVfjr3hKMEC6YC4bvn', {
+  host: 'postgres://postgres_service_nst5_user:rEkNxBelKINzMxKVfjr3hKMEC6YC4bvn@dpg-cpcmi3q1hbls73c8pl20-a.oregon-postgres.render.com/postgres_service_nst5',
   dialect: 'postgres'
 });
 
